9217b581296295e671b9009ffa7ad6ebf6983e88
juanmabec24 <62466088+juanmabec24@users.noreply.github.com>
2020-06-16 17:29:28 +0200

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,6 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 # Divide our dataset between train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -11,21 +12,13 @@ model.fit(X_train, y_train)
 # Predict the category for the test data based on the trained model 
 y_pred = model.predict(X_test)
 
-model = DecisionTreeClassifier()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-
+# Calculate the accuracy of the Decision Tree default model
 accuracy = model.score(X_test,y_test)
-print("Accuracy Linear: {}%".format(accuracy*100))
+print("Accuracy Tree: {}%".format(accuracy*100))
 
-
-from sklearn.metrics import confusion_matrix
-print(confusion_matrix(y_test, y_pred))
-
-
-# Compute confusion matrix
+# Compute the confusion matrix for the predicted output
 cnf_matrix = confusion_matrix(y_test, y_pred)
-# Plot confusion matrix using the plot_confusion_matrix function created in a separated file
+# Plot the confusion matrix using the function plot_confusion_matrix (in a separated file)
 plt.figure(figsize=(6, 6))
 plot_confusion_matrix(cnf_matrix, classes=["Non-defalut", "Default"])
 plt.savefig('confusion_matrix_raw.png', dpi=150)
